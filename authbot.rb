81dc1e1475f2eaa1d245771767a2262a1fd5ead2
michael verdi <michael.v.verdi@gmail.com>
2012-05-31 22:01:33 -0400

    added whitelist functionality

--- authbot.rb
+++ authbot.rb
@@ -1,8 +1,9 @@
 ENV['GOOGLE_AUTH_URL'] = 'https://www.google.com/accounts/o8/id'
 
 require 'bundler'
-Bundler.require 
+Bundler.require
 set :session_secret, 'HungryAcademy'
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/database.yml",
@@ -16,15 +17,29 @@ end
 
 get '/' do
   authenticate
-  user_info  = JSON.parse(session["user_info"])
-  user       = User.find_by_email(user_info["email"])
-  email      = user_info["email"] 
-  first_name = user_info["first_name"] 
-  last_name  = user_info["last_name"] 
 
-  unless user
-    User.create(first_name: first_name,
+  user_info  = JSON.parse(session["user_info"])
+  email      = user_info["email"]
+  first_name = user_info["first_name"]
+  last_name  = user_info["last_name"]
+
+  email_domain = email.split("@")[1]
+  valid_email_domains = ['hungryacademy.org', 'livingsocial.com']
+
+  user = User.find_by_email(user_info["email"])
+
+  if valid_email_domains.include?(email_domain)
+    unless user
+      User.create(first_name: first_name,
                 last_name: last_name,
                 email: email)
+    end
+  else
+    terminate_sessions
   end
+end
+
+def terminate_sessions
+  session["user_info"] = nil
+  session["user"] = nil
 end
