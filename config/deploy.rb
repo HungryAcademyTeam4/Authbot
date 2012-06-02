1d4d3d54c2ee57e36e74b2b01a694c9a9593ac55
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-02 17:17:46 -0400

    typo in the recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_via, :remote_cache
 after "deploy:setup", "deploy:create_release_dir"
 namespace :deploy do 
   task :create_release_dir, :except => {:no_release => true} do
-    run "mkdir -p #{fetch ;releases_path}"
+    run "mkdir -p #{fetch :releases_path}"
   end 
 end 
 
