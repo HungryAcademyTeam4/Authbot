2cab2275cdaf981a373cc5dd747209d0d1e2f7b9
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-02 17:23:25 -0400

    change deploy user to root

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 require 'bundler/capistrano'
 
 server "50.116.38.51", :web, :db, :app, primary: true
-set :user, "deployer"
+set :user, "root"
 set :application, "Authbot"
 
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/apps/#{application}"
 set :deploy_via, :remote_cache
 
 after "deploy:setup", "deploy:create_release_dir"
@@ -25,11 +25,11 @@ ssh_options[:forward_agent] = true
 namespace :deploy do
   desc "Start the Thin processes"
   task :bundle_install do
-    run "cd /home/#{user}/apps/#{application}/current && bundle install"
+    run "cd /home/apps/#{application}/current && bundle install"
   end
   task :start do
     bundle_install
-    run "cd /home/#{user}/apps/#{application}/current && bundle exec rake db:migrate"
-    run "cd /home/#{user}/apps/#{application}/current && bundle exec ruby ./authbot.rb -p 4568"
+    run "cd /home/apps/#{application}/current && bundle exec rake db:migrate"
+    run "cd /home/apps/#{application}/current && bundle exec ruby ./authbot.rb -p 4568"
   end
 end
