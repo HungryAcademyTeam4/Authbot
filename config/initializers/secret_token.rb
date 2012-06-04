85c1abea90adc200482dc7eff04442221b34d7d4
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-04 10:47:15 -0400

    scaffold rails app for auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authbot::Application.config.secret_token = '9a0c0abaab1079c097bf16c0ea8164aebd60402dcc06464a4911f66aaae29ca427218994cc826385d93a789ed1de50168b1daf6a1da516d40be87bfe5e91574f'
