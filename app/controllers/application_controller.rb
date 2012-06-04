9cfbf68c2004986c7b76ef52b9b9b9b5953dd90a
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-04 11:15:07 -0400

    config callback and google endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user 
+    if session ["user_id"]
+      user = User.find(session["user_id"])
+    end 
+    user
+  end 
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :first_name, :last_name, :email, :uid
 end
