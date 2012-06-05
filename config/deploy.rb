3229a343de62a793454b641a053d9d928eb641b7
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-05 18:18:47 -0400

    changed recipe for smooth deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,6 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, true
 
-# after "deploy:setup", "deploy:create_release_dir"
-# namespace :deploy do 
-#   task :create_release_dir, :except => {:no_release => true} do
-#     run "mkdir -p #{fetch :releases_path}"
-#   end 
-# end 
-
-
 set :scm, "git"
 set :repository,  "git://github.com/HungryAcademyTeam4/Authbot.git"
 set :branch, "master"
@@ -24,13 +16,26 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
+
+
 namespace :deploy do
   desc "Start the Thin processes"
   task :bundle_install do
     run "cd /home/#{user}/apps/#{application}/current && bundle install"
   end
+  task :create_release_log do
+    run <<-CMD
+    rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+    mkdir -p #{latest_release}/public &&
+    mkdir -p #{latest_release}/tmp &&
+    ln -s #{shared_path}/log #{latest_release}/log &&
+    ln -s #{shared_path}/system #{latest_release}/public/system &&
+    ln -s #{shared_path}/pids #{latest_release}/tmp/pids
+    CMD
+  end
   task :start do
     bundle_install
+    create_release_log
     run "cd /home/#{user}/apps/#{application}/current && bundle exec rake db:create && bundle exec rake db:migrate"
     run "cd /home/#{user}/apps/#{application}/current && bundle exec rails s -p 4568 -e production"
   end
