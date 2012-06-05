cb5708084a6b96ba97faf53a67bce58e5627dd14
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-05 15:15:04 -0400

    user can authenticate if email domain appears on whitelist

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationsController < ApplicationController
   def create
     @omniauth = request.env["omniauth.auth"]
-    @omniauth.inspect
+    @user = User.find_or_create_by_auth(@omniauth)
   end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :uid
+
+  def self.find_or_create_by_auth(auth)
+    return nil unless passes_whitelist?(auth)
+    user = User.find_by_uid(auth["uid"])
+    unless user 
+      user = User.create(:first_name => auth["info"]["first_name"], 
+                         :last_name => auth["info"]["last_name"],
+                         :email => auth["info"]["email"], 
+                         :uid => auth["uid"])
+    end 
+    user
+  end 
+
+  def self.passes_whitelist?(auth)
+    if auth["info"]["email"].split("@").last.upcase == "HUNGRYMACHINE.COM"
+      true
+    else 
+      false
+    end
+  end 
 end
