983fc2fe3f3a16c41fc0c4fc661b5590f42622f8
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-06-05 15:29:10 -0400

    Setting cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user 
-    if session ["user_id"]
-      user = User.find(session["user_id"])
+    if session["uid"]
+      user = User.find_by_uid(session["uid"])
     end 
     user
   end 
+
+  helper_method :current_user
 
 
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,5 +2,10 @@ class AuthenticationsController < ApplicationController
   def create
     @omniauth = request.env["omniauth.auth"]
     @user = User.find_or_create_by_auth(@omniauth)
+    set_cookie(@user) if @user 
+  end 
+
+  def set_cookie(user)
+    session["uid"] = user.uid
   end 
 end
