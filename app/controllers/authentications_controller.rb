1ae9333d45ca071e77fda4dc473ddee807c3bf26
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-06 16:26:50 -0400

    Attempt graceful redirect after auth

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,7 +2,8 @@ class AuthenticationsController < ApplicationController
   def create
     @omniauth = request.env["omniauth.auth"]
     @user = User.find_or_create_by_auth(@omniauth)
-    set_cookie(@user) if @user 
+    set_cookie(@user) if @user
+    return redirect_to '/'
   end 
 
   def destroy
